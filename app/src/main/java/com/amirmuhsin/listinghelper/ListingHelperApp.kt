c5487ae51663f06c0581051873c1bb23ff0d3cac
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2025-06-13 20:30:08 +0500

    - test CloudXSDK added

---
+++ app/src/main/java/com/amirmuhsin/listinghelper/ListingHelperApp.kt
@@ -0,0 +1,13 @@
+package com.amirmuhsin.listinghelper
+
+import android.app.Application
+
+class ListingHelperApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+
+    }
+
+}
--- app/src/main/java/com/amirmuhsin/listinghelper/MainActivity.kt
+++ app/src/main/java/com/amirmuhsin/listinghelper/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.amirmuhsin.listinghelper
 
 import androidx.activity.viewModels
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.amirmuhsin.listinghelper.core_views.base.ui.BaseActivity
 import com.amirmuhsin.listinghelper.databinding.ActivityMainBinding
+import io.cloudx.sdk.CloudX
+import io.cloudx.sdk.CloudXInitializationListener
+import io.cloudx.sdk.CloudXInitializationStatus
 
 class MainActivity: BaseActivity<ActivityMainBinding, MainViewModel>(ActivityMainBinding::inflate) {
 
@@ -14,6 +14,16 @@ class MainActivity: BaseActivity<ActivityMainBinding, MainViewModel>(ActivityMai
 
     private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment }
     private val navController by lazy { navHostFragment.navController }
+
+    override fun assignObjects() {
+
+        val initParams = CloudX.InitializationParams("3qo_qwMH0aaBXUb_rjmga", "https://pro-dev.cloudx.io/sdk", null)
+        CloudX.initialize(this, initParams, object: CloudXInitializationListener {
+            override fun onCloudXInitializationStatus(status: CloudXInitializationStatus) {
+                println("hop: SDK initialized")
+            }
+        })
+    }
 
     override fun onBackPressed() {
         if (!navController.popBackStack()) {
