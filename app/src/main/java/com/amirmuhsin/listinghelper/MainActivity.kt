e4865da1a2d88f62a2b9ac4a99ca3edba20692c9
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2025-07-27 19:50:04 +0500

    - removed CloudX

--- app/src/main/java/com/amirmuhsin/listinghelper/MainActivity.kt
+++ app/src/main/java/com/amirmuhsin/listinghelper/MainActivity.kt
@@ -4,10 +4,6 @@ import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import com.amirmuhsin.listinghelper.core_views.base.ui.BaseActivity
 import com.amirmuhsin.listinghelper.databinding.ActivityMainBinding
-import com.amirmuhsin.listinghelper.ui.s1_home.HomeFragment
-import io.cloudx.sdk.CloudX
-import io.cloudx.sdk.CloudXInitializationListener
-import io.cloudx.sdk.CloudXInitializationStatus
 
 class MainActivity: BaseActivity<ActivityMainBinding, MainViewModel>(ActivityMainBinding::inflate) {
 
@@ -18,24 +14,6 @@ class MainActivity: BaseActivity<ActivityMainBinding, MainViewModel>(ActivityMai
 
     override fun assignObjects() {
 
-        val initParams = CloudX.InitializationParams("3qo_qwMH0aaBXUb_rjmga", "https://pro-dev.cloudx.io/sdk", null)
-        CloudX.initialize(this, initParams, object: CloudXInitializationListener {
-            override fun onCloudXInitializationStatus(status: CloudXInitializationStatus) {
-                if (status.initialized) {
-                    println("hop: CloudX initialized successfully")
-                } else {
-                    println("hop: CloudX initialization failed with error: ${status.description}")
-                }
-
-                // access HomeFragment of this MainActivity
-                val homeFragment = navHostFragment.childFragmentManager.fragments.firstOrNull { it is HomeFragment } as? HomeFragment
-                if (homeFragment != null) {
-//                    homeFragment.initAds()
-                } else {
-                    println("hop: HomeFragment not found in NavHostFragment")
-                }
-            }
-        })
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/amirmuhsin/listinghelper/ui/s1_home/HomeFragment.kt
+++ app/src/main/java/com/amirmuhsin/listinghelper/ui/s1_home/HomeFragment.kt
@@ -1,6 +1,5 @@
 package com.amirmuhsin.listinghelper.ui.s1_home
 
-import android.R.attr.versionCode
 import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.fragment.app.viewModels
@@ -9,56 +8,12 @@ import com.amirmuhsin.listinghelper.R
 import com.amirmuhsin.listinghelper.core_views.base.ui.BaseFragment
 import com.amirmuhsin.listinghelper.data.networking.PhotoRoomNetworkModule
 import com.amirmuhsin.listinghelper.databinding.FragmentHomeBinding
-import io.cloudx.sdk.AdViewListener
-import io.cloudx.sdk.CloudX
-import io.cloudx.sdk.CloudXAd
-import io.cloudx.sdk.CloudXAdError
 
 class HomeFragment: BaseFragment<FragmentHomeBinding, HomeViewModel>(
     FragmentHomeBinding::inflate
 ) {
 
     override val viewModel: HomeViewModel by viewModels()
-
-    fun initAds() {
-        val bannerAd = CloudX.createBanner(requireActivity(), "pl-android", listener = object: AdViewListener {
-            override fun onAdClosedByUser(placementName: String) {
-                println("hop: Ad closed by user for placement: $placementName")
-            }
-
-            override fun onAdClicked(cloudXAd: CloudXAd) {
-                println("hop: Ad clicked")
-            }
-
-            override fun onAdHidden(cloudXAd: CloudXAd) {
-                println("hop: Ad hidden")
-            }
-
-            override fun onAdLoadFailed(cloudXAdError: CloudXAdError) {
-                println("hop: Ad load failed")
-            }
-
-            override fun onAdLoadSuccess(cloudXAd: CloudXAd) {
-                println("hop: Ad load success")
-            }
-
-            override fun onAdShowFailed(cloudXAdError: CloudXAdError) {
-                println("hop: Ad show failed")
-            }
-
-            override fun onAdShowSuccess(cloudXAd: CloudXAd) {
-                println("hop: Ad show success")
-            }
-        })
-        if (bannerAd == null) {
-            println("hop: Failed to create banner ad")
-            return
-        } else {
-            println("hop: Banner ad created successfully")
-        }
-        binding.flBannerAd.addView(bannerAd)
-        bannerAd.show()
-    }
 
     override fun assignObjects() {
         binding.chSandbox.isChecked = PhotoRoomNetworkModule.isSandbox
