24ad07fcba6e759096375dc34930761cbb33b27b
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2025-08-10 17:55:08 +0500

    - added status bar

--- app/src/main/java/com/amirmuhsin/listinghelper/ui/s2_0_product_detail/list/CleanedPhotoItemLayout.kt
+++ app/src/main/java/com/amirmuhsin/listinghelper/ui/s2_0_product_detail/list/CleanedPhotoItemLayout.kt
@@ -3,10 +3,13 @@ package com.amirmuhsin.listinghelper.ui.s2_0_product_detail.list
 import android.content.Context
 import android.view.LayoutInflater
 import android.widget.FrameLayout
+import androidx.core.view.isVisible
 import coil.load
 import coil.size.ViewSizeResolver
+import com.amirmuhsin.listinghelper.R
 import com.amirmuhsin.listinghelper.databinding.ItemLayoutCleanedPhotoBinding
 import com.amirmuhsin.listinghelper.domain.photo.PhotoPair
+import com.amirmuhsin.listinghelper.domain.photo.PhotoPair.UploadStatus
 
 class CleanedPhotoItemLayout(
     context: Context,
@@ -35,6 +38,27 @@ class CleanedPhotoItemLayout(
         binding.ivCleanedThumb.load(pair.cleanedUri) {
             size(ViewSizeResolver(binding.ivCleanedThumb))
             crossfade(true)
+        }
+
+        when (pair.uploadStatus) {
+            UploadStatus.PENDING -> {
+                binding.ivUploadStatus.isVisible = true
+                binding.ivUploadStatus.setImageResource(R.drawable.ic_upload)
+            }
+
+            UploadStatus.UPLOADED -> {
+                binding.ivUploadStatus.isVisible = true
+                binding.ivUploadStatus.setImageResource(R.drawable.ic_check)
+            }
+
+            UploadStatus.FAILED -> {
+                binding.ivUploadStatus.isVisible = true
+                binding.ivUploadStatus.setImageResource(R.drawable.ic_warning)
+            }
+
+            else -> {
+
+            }
         }
     }
 }
