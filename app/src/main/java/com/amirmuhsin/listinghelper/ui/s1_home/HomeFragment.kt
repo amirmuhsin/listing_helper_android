71a24861029eb497254c60500f15fc1556116c07
Mirmukhsin Sodikov <mirmuhsin.sodiqov@gmail.com>
2025-10-08 15:18:09 +0500

    1.0.6

--- app/src/main/java/com/amirmuhsin/listinghelper/ui/s1_home/HomeFragment.kt
+++ app/src/main/java/com/amirmuhsin/listinghelper/ui/s1_home/HomeFragment.kt
@@ -77,7 +77,7 @@ class HomeFragment: BaseFragment<FragmentHomeBinding, HomeViewModel>(
         val packageInfo = packageManager.getPackageInfo(packageName, 0)
         val versionName = packageInfo.versionName
 
-        binding.tvVersion.text = "Version: $versionName | 11-August-2025"
+        binding.tvVersion.text = "Version: $versionName | 8-October-2025"
 
         binding.rvProducts.adapter = productListAdapter
         binding.rvProducts.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
